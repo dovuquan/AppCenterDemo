8f0b1cee0949cf7d245be7bcb90d1352cb7d90d3
Quan Do <quan.do@asnet.com.vn>
2023-04-17 21:16:39 +0700

    Config build for ios

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+ import {AppRegistry} from 'react-native';
+ import App from './apps/mobile/src/app/App';
+ import {name as appName} from './apps/mobile/app.json';
+ 
+ AppRegistry.registerComponent(appName, () => App);
+ 
